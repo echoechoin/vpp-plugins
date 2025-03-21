87c2c5484bfb56b9492b5a7b1d6671dcc419db4d
WanBingjiang <614699596@qq.com>
2025-03-21 14:15:22 +0800

    impove: change indent mode to TAB

--- cpu_loading/cpu_loading.h
+++ cpu_loading/cpu_loading.h
@@ -7,37 +7,37 @@
 #define DEFAULT_SAMPLE_TIME 1.5
 
 #define for_each_poll_type_node \
-    _(dpdk) \
-    _(virtio) \
+	_(dpdk) \
+	_(virtio) \
 
 enum {
 #define _(n) POLLING_NODE_##n,
-    for_each_poll_type_node
+	for_each_poll_type_node
 #undef _
-    POLLING_NODE_MAX,
+	POLLING_NODE_MAX,
 };
 
 static const char *poll_type_str[] = {
 #define _(n) #n "-input",
-    for_each_poll_type_node
+	for_each_poll_type_node
 #undef _
 };
 
 typedef struct {
-    vlib_main_t *vlib_main;
+	vlib_main_t *vlib_main;
 
-    /* 用于计算 cpu loading 的参数 */
-    u64 *idle_time[POLLING_NODE_MAX];
-    u64 *total_time;
+	/* for caculating cpu loading */
+	u64 *idle_time[POLLING_NODE_MAX];
+	u64 *total_time;
 
-    /* 采样时间 */
-    f64 sampling_time;
+	/* sampling time */
+	f64 sampling_time;
 
-    /* 每隔 <sampling_time> seconds 计算一次 cpu loading */
-    float *last_cpu_loading;
+	/* last cpu loading */
+	float *last_cpu_loading;
 
-    /* 每个线程 polling 节点的 ID */
-    u32 *polling_node_index[POLLING_NODE_MAX];
+	/* the node id of polling node each thread */
+	u32 *polling_node_index[POLLING_NODE_MAX];
 } cpu_loading_main_t;
 
 extern cpu_loading_main_t cpu_loading_main;
--- cpu_loading/cpu_loading_cli.c
+++ cpu_loading/cpu_loading_cli.c
@@ -3,53 +3,53 @@
 #include "vlib/cli.h"
 
 /**
- * @brief 查询CPU使用率
+ * @brief show cpu loading
  **/
 clib_error_t *show_cpu_loading(struct vlib_main_t * vm,
-        unformat_input_t * input, struct vlib_cli_command_t * cmd)
+		unformat_input_t * input, struct vlib_cli_command_t * cmd)
 {
-    int i, j;
-    clib_error_t *error = 0;
-    cpu_loading_main_t *cpm = &cpu_loading_main;
+	int i, j;
+	clib_error_t *error = 0;
+	cpu_loading_main_t *cpm = &cpu_loading_main;
 
-    vlib_cli_output(vm, "sampling time: %.2f", cpm->sampling_time);
-    vlib_cli_output(vm, "---------------------", cpm->sampling_time);
-    for (i = 0; i < vlib_get_n_threads(); i++) {
-        vlib_cli_output(vm, "thread %u:", i);
-        for (j = 0; j < POLLING_NODE_MAX; j++) {
-            if (cpm->polling_node_index[j][i] == ~0)
-                continue;
-            vlib_cli_output(vm, "    %s %s %lu", poll_type_str[j], "idle clock:", cpm->idle_time[j][i]);
-        }
-        vlib_cli_output(vm, "    %s %lu", "total clock:", cpm->total_time[i]);
-        vlib_cli_output(vm, "    %s %.2f%%", "last cpu loading:", cpm->last_cpu_loading[i]);
-    }
-    return error;
+	vlib_cli_output(vm, "sampling time: %.2f", cpm->sampling_time);
+	vlib_cli_output(vm, "---------------------", cpm->sampling_time);
+	for (i = 0; i < vlib_get_n_threads(); i++) {
+		vlib_cli_output(vm, "thread %u:", i);
+		for (j = 0; j < POLLING_NODE_MAX; j++) {
+			if (cpm->polling_node_index[j][i] == ~0)
+				continue;
+			vlib_cli_output(vm, "	%s %s %lu", poll_type_str[j], "idle clock:", cpm->idle_time[j][i]);
+		}
+		vlib_cli_output(vm, "	%s %lu", "total clock:", cpm->total_time[i]);
+		vlib_cli_output(vm, "	%s %.2f%%", "last cpu loading:", cpm->last_cpu_loading[i]);
+	}
+	return error;
 }
 
 /**
- * @brief 设置采样时间
+ * @brief set sampling time
  **/
 clib_error_t *cpu_loading_set_sampling_time(struct vlib_main_t *vm,
-        unformat_input_t *input, struct vlib_cli_command_t *cmd)
+		unformat_input_t *input, struct vlib_cli_command_t *cmd)
 {
-    clib_error_t *error = 0;
-    cpu_loading_main_t *cpm = &cpu_loading_main;
+	clib_error_t *error = 0;
+	cpu_loading_main_t *cpm = &cpu_loading_main;
 
-    if (unformat(input, "%f", &cpm->sampling_time) == 0)
-        error = clib_error_return(0, "please input a valid sampling time");
-    return error;
+	if (unformat(input, "%f", &cpm->sampling_time) == 0)
+		error = clib_error_return(0, "please input a valid sampling time");
+	return error;
 }
 
 VLIB_CLI_COMMAND (show_cpu_loading_command, static) = {
-    .path = "show cpu-loading",
-    .short_help = "Show commands",
-    .function = show_cpu_loading,
+	.path = "show cpu-loading",
+	.short_help = "Show commands",
+	.function = show_cpu_loading,
 };
 
 VLIB_CLI_COMMAND (set_cpu_loading_sampling_time_command, static) = {
-    .path = "set cpu-loading sampling-time",
-    .long_help = "set cpu-loading <float>",
-    .short_help = "set sampling time",
-    .function = cpu_loading_set_sampling_time,
+	.path = "set cpu-loading sampling-time",
+	.long_help = "set cpu-loading <float>",
+	.short_help = "set sampling time",
+	.function = cpu_loading_set_sampling_time,
 };
